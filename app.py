989544157e6558f946ac45ed52b56747c31b9e86
adilsardar2 <adilsardar1100@gmail.com>
2025-05-03 15:11:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -34,4 +34,6 @@ def receive():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run()
+    import os
+
+app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
